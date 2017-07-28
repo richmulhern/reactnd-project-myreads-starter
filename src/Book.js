f84e9b22126db92ef59012cc9dde1429a95deea0
Richard Mulhern <rmulhern@BSD-MBPL15-BR01.fios-router.home>
2017-07-28 08:28:39 -0400

    feat: Book Component Now Defaults to Shelf in Dropdown
    The dropdown item on a book now will default to the shelf value in the BooksAPI returns.

--- src/Book.js
+++ src/Book.js
@@ -10,10 +10,10 @@ function Book(props) {
             <div className="book-shelf-changer">
               <select>
                 <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
+                <option value="currentlyReading" selected={book.shelf === 'currentlyReading'}>Currently Reading</option>
+                <option value="wantToRead" selected={book.shelf === 'wantToRead'}>Want to Read</option>
+                <option value="read" selected={book.shelf === 'read'}>Read</option>
+                <option value="none" selected={book.shelf === 'none'}>None</option>
               </select>
             </div>
           </div>
