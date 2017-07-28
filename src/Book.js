83617ed18ca0e5cf60fe2b52e8cfedca3193d9e0
Richard Mulhern <rmulhern@BSD-MBPL15-BR01.fios-router.home>
2017-07-28 08:33:25 -0400

    refactor: Removed selected for defaultValue Property in Select on Book
    After getting errors in the console, I removed the selected property from each option in the
    Book component and set it on the select with the defaultValue property instead.

--- src/Book.js
+++ src/Book.js
@@ -8,12 +8,12 @@ function Book(props) {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select defaultValue={book.shelf}>
                 <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading" selected={book.shelf === 'currentlyReading'}>Currently Reading</option>
-                <option value="wantToRead" selected={book.shelf === 'wantToRead'}>Want to Read</option>
-                <option value="read" selected={book.shelf === 'read'}>Read</option>
-                <option value="none" selected={book.shelf === 'none'}>None</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
               </select>
             </div>
           </div>
